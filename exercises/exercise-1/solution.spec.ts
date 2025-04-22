93267dfc51f399649a9d33016e84bb1e4d967545
Tứ Linh <tranthitulinh1305@gmail.com>
2025-04-22 11:24:43 +0700

    update problem 1

--- exercises/exercise-1/solution.spec.ts
+++ exercises/exercise-1/solution.spec.ts
@@ -1,6 +1,7 @@
 /* JAVASCRIPT */
 function calculateBMI(height: number, weight: number) {
-  const BMI = weight / (height * height);
+  const BMI = weight / Math.pow(height, 2);
+
   if (BMI < 18.5) {
     console.log(`Kết quả BMI: ${BMI}\nPhân loại: Gầy`);
   } else if (BMI < 24.9) {
@@ -15,12 +16,17 @@ function calculateBMI(height: number, weight: number) {
 calculateBMI(1.75, 68);
 
 /* PLAYWRIGHT */
-import { test, expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
+
+// test.beforeEach(async ({ page }) => {
+//   await page.goto("https://material.playwrightvn.com/");
+// });
 
 test("problem 1", async ({ page }) => {
+  await page.goto("/");
+
   // Step 1: Navigate to the Register Page
-  await page.goto("https://material.playwrightvn.com/");
-  await page.getByRole("link", { name: /Register Page/ }).click();
+  await page.getByRole("link", { name: "Register" }).click();
 
   // Step 2: Fill in Registration Details
   const username = "mni-linh";
@@ -30,18 +36,21 @@ test("problem 1", async ({ page }) => {
   await page.locator("#email").fill(email);
 
   // Step 3: Submit the Registration Form
-  await page.getByRole("link", { name: "Register" }).click();
+  await page.getByRole("button", { name: "Register" }).click();
 
-  // Step 4: Validate Navigation to the Register Page
+  // Check URL after registration
   await expect(page).toHaveURL(
     "https://material.playwrightvn.com/01-xpath-register-page.html"
   );
 
-  // Step 5: Validate Registration Table
-  // Ensure only one row exists in the table
-  await expect(page.locator("//tbody//tr")).toHaveCount(1);
+  // Get the count of rows in the table
+  const rowCount = await page.locator("//tbody/tr").count();
+  // Get the last row in the table
+  const lastRow = page.locator("//tbody/tr[last()]");
 
-  // Validate Username and Email in the table
-  await expect(page.locator("//tbody//td").nth(1)).toHaveText(username);
-  await expect(page.locator("//tbody//td").nth(2)).toHaveText(email);
+  // Validate the number of rows in the table
+  await expect(lastRow.locator("td").nth(0)).toHaveText(rowCount.toString());
+  // Validate the username and email in the last row of the table
+  await expect(lastRow.locator("td").nth(1)).toHaveText(username);
+  await expect(lastRow.locator("td").nth(2)).toHaveText(email);
 });
--- exercises/temp/temp.spec.ts
+++ exercises/temp/temp.spec.ts
@@ -16,9 +16,41 @@ function calculateBMI(height: number, weight: number) {
 calculateBMI(1.75, 68);
 
 /* PLAYWRIGHT */
-import { test } from "@playwright/test";
-test("problem 1", async ({ page }) => {
-  await page.goto("https://material.playwrightvn.com/");
+import { expect, test } from "@playwright/test";
 
+// test.beforeEach(async ({ page }) => {
+//   await page.goto("https://material.playwrightvn.com/");
+// });
+
+test("problem 1", async ({ page }) => {
+  await page.goto("/");
+
+  // Step 1: Navigate to the Register Page
   await page.getByRole("link", { name: "Register" }).click();
+
+  // Step 2: Fill in Registration Details
+  const username = "mni-linh";
+  const email = "tranthitulinh1305@gmail.com";
+
+  await page.locator("#username").fill(username);
+  await page.locator("#email").fill(email);
+
+  // Step 3: Submit the Registration Form
+  await page.getByRole("button", { name: "Register" }).click();
+
+  // Check URL after registration
+  await expect(page).toHaveURL(
+    "https://material.playwrightvn.com/01-xpath-register-page.html"
+  );
+
+  // Get the count of rows in the table
+  const rowCount = await page.locator("//tbody/tr").count();
+  // Get the last row in the table
+  const lastRow = page.locator("//tbody/tr[last()]");
+
+  // Validate the number of rows in the table
+  await expect(lastRow.locator("td").nth(0)).toHaveText(rowCount.toString());
+  // Validate the username and email in the last row of the table
+  await expect(lastRow.locator("td").nth(1)).toHaveText(username);
+  await expect(lastRow.locator("td").nth(2)).toHaveText(email);
 });
